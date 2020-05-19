f4d2cf3920c89c371bc6e4478a0e21ca338ce308
Faizan <mirzafaizanejaz@gmail.com>
2020-05-19 15:33:27 +0300

    added PWA functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,32 @@ module.exports = {
         contentPath: "content/",
         showThemeLogo: false,
         theme: "dark-blue",
-      },
+      }
     },
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `Mirza Faizan`,
+          short_name: `mirzafaizan`,
+          start_url: `/`,
+          background_color: `#0000ff`,
+          theme_color: `#0000ff`,
+          display: `standalone`,
+          icon: `content/images/logo.svg`, // This path is relative to the root of the site.
+        },
+      },
+      {
+        resolve: `gatsby-plugin-offline`,
+        options: {
+          precachePages: [`/`]
+        },
+      },
+      {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+          // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+          trackingId: "UA-167100745-1",
+        },
+      },
   ],
 }
